313dc4d2ff48e3bfdadacd6d37c739fb1b316413
김희진 <95160590+Khjin06k@users.noreply.github.com>
2022-09-19 19:19:27 +0900

    [Silver V] Title: 셀프 넘버, Time: 156 ms, Memory: 14532 KB -BaekjoonHub

--- 백준/Silver/4673. 셀프 넘버/셀프 넘버.java
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.java
@@ -1,24 +1,22 @@
 public class Main {
-	public static int self(int num) {
-		int sum=num;
-		while(num!=0) {
-			sum += num%10;
-			num/=10;
-		}
-		return sum;
-	}
-	public static void main(String[] args) {
-		boolean[] arr = new boolean[10001];
-		for(int i=1;i<10001;i++) {
-			int selfnum=self(i);
-			if(selfnum<=10000) {
-				arr[selfnum]=true;
-			}
-		}
-		for(int i=1;i<=10000;i++) {
-			if(!arr[i]) {
-				System.out.println(i);
-			}
-		}
-	}
+    public static int d(int num){ //각자리 숫자를 더하는 메서드 생성
+        int sum = num;
+        while(num !=0){
+            sum += num%10;
+            num/=10;
+        }
+        return sum;
+    }
+    public static void main(String[] args) {
+        boolean[] arr = new boolean[10001];
+        for(int i = 1; i<=10000; i++){
+            int n = d(i); //메서드에 i를 넣은 것을 n에 저장
+            if(n<=10000){ //함수 값이 1보다 작으면 배열에 true를 저장
+                arr[n] = true;
+            }
+        }
+        for(int i = 1; i<=10000; i++){//배열이 false가 있으면 해당 값 출력
+            if(arr[i]==false) System.out.println(i);
+        }
+    }
 }
