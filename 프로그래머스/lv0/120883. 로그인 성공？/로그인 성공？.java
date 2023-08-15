1cc8f9e7024ce89b8103f0eb83fc82811e49f7d3
김희진 <95160590+Khjin06k@users.noreply.github.com>
2023-08-15 14:58:36 +0900

    [level 0] Title: 로그인 성공?, Time: 0.04 ms, Memory: 77.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String solution(String[] id_pw, String[][] db) {
+        String answer = "";
+        for(int i = 0; i<db.length; i++){
+            if((db[i][0].equals(id_pw[0])) && (db[i][1].equals(id_pw[1]))){
+                answer = "login";
+                break;
+            } 
+            if((db[i][0].equals(id_pw[0])) && (!db[i][1].equals(id_pw[1]))) answer = "wrong pw";
+        }
+        if(answer.equals("")) return "fail";
+        return answer;
+    }
+}
